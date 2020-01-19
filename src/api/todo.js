d848d30124a8abb0286d8d351229e9e58a4c4ba5
318Thorne <c3186564@uon.edu.au>
2020-01-19 20:55:44 +1100

    lesson-2: learning apis

--- src/api/todo.js
+++ src/api/todo.js
@@ -2,7 +2,7 @@ module.exports.handler = (event, ctx, done) => {
   done(null, {
     statusCode: 200,
     body: JSON.stringify({data: {
-      id: 1, name: 'clean up', status: 'open'
+      id: event.pathParameters.id, name: 'clean up', status: 'open'
     }})
   })
 }
